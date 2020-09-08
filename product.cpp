201c0ee42262e65bb3c4ae6f3b16809663f147d1
Sanskritimalik9 <67856098+Sanskritimalik9@users.noreply.github.com>
2020-09-09 00:47:06 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x, y, product;
+	cout<<"Enter two numbers:";
+	cin>>x>>y;
+	product = x*y;
+	cout<<x<<"*"<<y<<"="<<product;
+	return 0;
+}
---
+++ values.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Size of char is"<<sizeof(char)<<"bytes";
+	cout<<"Size of int is"<<sizeof(int)<<"byte";
+	cout<<"Size of float is"<<sizeof(float)<<"bytes";
+	cout<<"Size of double is"<<sizeof(double)<<"bytes";
+	return 0;
+	
+}
+
